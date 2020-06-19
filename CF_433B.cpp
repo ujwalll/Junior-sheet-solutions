4bc84d75dad17ac6ff96bbce3c0017b2ac799e42
ujwal1501 <ujwal@iitg.ac.in>
2020-06-19 23:07:57 +0530

    Add existing files

---
+++ CF_433B.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+using namespace std; 
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define for1(i,n) for(int i=1;i<=n;i++)
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define FORD(i,a,b) for(int i=(a);i>=(b);i--)
+const int INF = 1<<29;
+const int MOD=1000000007;
+#define pp pair<ll,ll>
+#define int long long int 
+bool isPowerOfTwo (int x)  
+{  
+    return x && (!(x&(x-1)));  
+}
+void fastio()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+}
+long long binpow(long long a, long long b) {
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+const int dx[] = {1,0,-1,0,1,1,-1,-1};
+const int dy[] = {0,-1,0,1,1,-1,-1,1};
+////////////////////////////////////////////////////////////////////
+
+signed main()
+{
+  fastio();
+  int t=1;
+  //cin>>t;
+  while(t--)
+  {
+    int n;
+    cin>>n;
+    int a[n+1],sum[n+1];
+    a[0]=-1;
+    sum[0]=0;
+    for1(i,n)cin>>a[i];
+    for1(i,n)
+    {
+        sum[i]=sum[i-1]+a[i];
+    }
+    sort(a,a+n+1);
+    a[0]=0;
+    for1(i,n)
+    {
+        a[i]=a[i]+a[i-1];
+    }
+    int q;
+    cin>>q;
+    while(q--)
+    {
+        int typ,l,r;
+        cin>>typ>>l>>r;
+        if(typ==2)cout<<a[r]-a[l-1];
+        else cout<<sum[r]-sum[l-1];
+        cout<<"\n";
+    }
+  }  
+  return 0;
+  //Read the stuff at the bottom
+}
+/* Look for:
+* the exact constraints (multiple sets are too slow for n=10^6 :( )
+* special cases (n=1?)
+* 1LL<<i and not 1<<i
+* overflow (int vs ll?)
+* array bounds
+* if you have no idea just guess the appropriate well-known algo instead of doing nothing :/
+*/
+/*lcm(gcd(N1, M), gcd(N2, M), ..., gcd(Nk, M)) = gcd(lcm(N1, ..., Nk), M)
+gcd(lcm(N1, M), lcm(N2, M), ..., lcm(Nk, M)) = lcm(gcd(N1, ..., Nk), M).
+
+If gcd(N1, N2) = 1, then
+gcd(N1·N2, M) = gcd(N1, M)·gcd(N2, M)
+lcm(N1·N2, M) = lcm(N1, M)·lcm(N2, M)/M.
+
+lcm(M, N, P) · gcd(M, N) · gcd(N, P) · gcd(P, M) = NMP · gcd(N, M, P).
+*/
---
+++ CF_451B.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+using namespace std; 
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define for1(i,n) for(int i=1;i<=n;i++)
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define FORD(i,a,b) for(int i=(a);i>=(b);i--)
+const int INF = 1<<29;
+const int MOD=1000000007;
+#define pp pair<ll,ll>
+typedef long long int ll;
+bool isPowerOfTwo (ll x)  
+{  
+    return x && (!(x&(x-1)));  
+}
+void fastio()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+}
+long long binpow(long long a, long long b) {
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+const int dx[] = {1,0,-1,0,1,1,-1,-1};
+const int dy[] = {0,-1,0,1,1,-1,-1,1};
+////////////////////////////////////////////////////////////////////
+
+int main()
+{
+  fastio();
+  int t=1;
+  //cin>>t;
+  while(t--)
+  {
+    int n;
+    cin>>n;
+    int a[n],c[n];
+    REP(i,n)
+    {
+        cin>>a[i];
+        c[i]=a[i];
+    }
+    sort(c,c+n);
+    int x=0;
+    while(x<n-1 && a[x+1]>a[x])x++;
+    if(x==n-1)
+    {
+        cout<<"yes\n"<<"1 1";
+    }
+    else
+    {
+        int y=x+1;
+        while(y<n-1 && a[y]>a[y+1])y++;
+        reverse(a+x,a+y+1);
+        REP(i,n)
+        {
+            if(a[i]!=c[i])
+            return cout<<"no",0;
+        }
+        cout<<"yes\n";
+        cout<<x+1<<" "<<y+1;
+    }
+    
+
+  }  
+  return 0;
+  //Read the stuff at the bottom
+}
+/* Look for:
+* the exact constraints (multiple sets are too slow for n=10^6 :( )
+* special cases (n=1?)
+* 1LL<<i and not 1<<i
+* overflow (int vs ll?)
+* array bounds
+* if you have no idea just guess the appropriate well-known algo instead of doing nothing :/
+*/
+/*lcm(gcd(N1, M), gcd(N2, M), ..., gcd(Nk, M)) = gcd(lcm(N1, ..., Nk), M)
+gcd(lcm(N1, M), lcm(N2, M), ..., lcm(Nk, M)) = lcm(gcd(N1, ..., Nk), M).
+
+If gcd(N1, N2) = 1, then
+gcd(N1·N2, M) = gcd(N1, M)·gcd(N2, M)
+lcm(N1·N2, M) = lcm(N1, M)·lcm(N2, M)/M.
+
+lcm(M, N, P) · gcd(M, N) · gcd(N, P) · gcd(P, M) = NMP · gcd(N, M, P).
+*/
